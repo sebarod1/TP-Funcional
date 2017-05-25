e2ad4aff43a7ccbb34e7303a126f66d53a876745
Juan M. Altamirano <juanmaalt@gmail.com>
2017-05-25 19:45:23 -0300

    Update TP.hs
    Code update

--- TP.hs
+++ TP.hs
@@ -7,7 +7,7 @@ type Nombre      = String
 type Resistencia = Int
 type Amigos      = [Nombre]
 data Cliente     = Cliente Nombre Resistencia Amigos deriving (Show, Eq)
---data Cliente = Cliente { Nombre::String, Resistencia::Int, Amigos::[Nombre] } deriving (Show)
+--data Cliente       = Cliente { Nombre :: String, resistencia :: Int, amigos :: [nombre] } deriving (Show)
 
 nombre (Cliente nom _ _ ) = nom
 resistencia (Cliente _ res _ ) = res
@@ -23,16 +23,16 @@ cristian = Cliente "Cristian" 2 []
 ana      = Cliente "Ana" 120 ["Marcos", "Rodri"] 
 
 -- Funcion comoEsta (Punto 3)
-comoEsta unCliente | (resistencia unCliente >= 50) = (nombre unCliente) ++ "fresco"
-                   | (length (amigos unCliente) >= 2) = (nombre unCliente) ++ "piola"
-                   | otherwise = (nombre unCliente) ++  "duro"
+comoEsta unCliente | (resistencia unCliente >= 50) = (nombre unCliente) ++ " fresco"
+                   | (length (amigos unCliente) >= 2) = (nombre unCliente) ++ " piola"
+                   | otherwise = (nombre unCliente) ++  " duro"
 
 -- Reconocer a un cliente como amigo (Punto 4)
 esAmigo unCliente otroCliente = elem (nombre unCliente) (amigos otroCliente)
 
 -- Bebidas (Punto 5)
 cambiarResistencia :: Int -> Cliente -> Cliente
-cambiarResistencia valor unCliente = (Cliente (nombre unCliente) valor (amigos unCliente))
+cambiarResistencia valor unCliente = (Cliente (nombre unCliente) (resistencia unCliente + valor) (amigos unCliente))
 
 cambiarNombre :: String -> Cliente -> Cliente 
 cambiarNombre nuevonombre unCliente = (Cliente nuevonombre (resistencia unCliente) (amigos unCliente))
@@ -41,9 +41,9 @@ calcularRs :: Int -> String
 calcularRs cantidad = replicate cantidad 'r' 
 
 grogXD unCliente         = cambiarResistencia 0 unCliente
-jarraloca unCliente      = cambiarResistencia (resistencia unCliente - 10) unCliente 
-klusener gusto unCliente = cambiarResistencia ((resistencia unCliente) - (length gusto)) unCliente
-tintico unCliente        = cambiarResistencia (resistencia unCliente + 5 * (length (amigos unCliente))) unCliente
+jarraloca unCliente      = cambiarResistencia (-10) unCliente 
+klusener gusto unCliente = cambiarResistencia (-(length gusto)) unCliente
+tintico unCliente        = cambiarResistencia (5 * length (amigos unCliente)) unCliente
 soda fuerza unCliente    = cambiarNombre ("e" ++ (calcularRs fuerza) ++ "p" ++ (nombre unCliente)) unCliente
 
 --Funcion Rescatarse (Punto 6)
